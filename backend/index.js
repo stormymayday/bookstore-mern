49734384c1530d400f607e183eaff13205821265
stormymayday <stormymayday7@gmail.com>
2024-01-13 11:23:59 -0800

    Added route for '/'

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { PORT } from "./config.js";
 
+// Defining a variable for Express
 const app = express();
 
+// Function for listening to a port
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
+});
+
+// Route for "/"
+app.get("/", (request, response) => {
+    console.log(request);
+
+    return response.status(234).send("Welcome to MERN Stack Tutorial");
 });

a7c8e090e9945c6f5f59b9d2653b911ba8474734
stormymayday <stormymayday7@gmail.com>
2024-01-14 21:40:01 -0800

    installed mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,9 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
 
 // Defining a variable for Express
 const app = express();
-
-// Function for listening to a port
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
 
 // Route for "/"
 app.get("/", (request, response) => {
@@ -15,3 +11,18 @@ app.get("/", (request, response) => {
 
     return response.status(234).send("Welcome to MERN Stack Tutorial");
 });
+
+// Connecting to MongoDB
+mongoose
+    .connect()
+    .then(() => {
+        console.log("App connected to database");
+
+        // Function for listening to a port
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error(error);
+    });
